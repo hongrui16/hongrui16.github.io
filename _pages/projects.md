7d5a7688a14d4130e94f944b8278e7b780e0dca7
hongrui16 <smarthong@qq.com>
2026-03-19 06:28:03 +0000

    Deploying to gh-pages from @ hongrui16/hongrui16.github.io@353feec34fc758e820ee327c47fc387b53cc7de0 🚀

--- _pages/projects.md
+++
@@ -1,4 +0,0 @@
-{% for project in site.data.projects %}
-**[{{ project.title }}]({{ project.url }})** — {{ project.description }}
-
-{% endfor %}
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
